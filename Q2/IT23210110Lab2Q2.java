bee1506d027f4b726b0ae08eb84ee48f2d2091d5
IT23210110 <IT23210110@my.sliit.lk>
2026-09-16 21:24:46 +0530

    Add files via upload

---
+++ Q2/IT23210110Lab2Q2.java
@@ -0,0 +1,13 @@
+public class IT23210110Lab2Q2{
+             public static void main(String[] args){
+			 double side = 10;
+			 
+			 double pi = 3.14;
+			 
+			 double squarePerimeter = 4 * side;
+			 double radius = squarePerimeter / (2*pi);
+			 
+			 System.out.println("Radius of the circular fence :" +radius);
+			 
+			 }
+			 }
