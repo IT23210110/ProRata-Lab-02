7d634753f7a7dc0cc7535c2f29f4f2aac1b65611
IT23210110 <IT23210110@my.sliit.lk>
2026-09-16 21:07:59 +0530

    Add files via upload

---
+++ Q1/IT23210110Lab2Q1.java
@@ -0,0 +1,20 @@
+public class IT23210110Lab2Q1{
+            public static void main(String[] args){
+			       double perimeter = 100;
+				   
+				   double length = perimeter/2/(1+3.0/4);
+				   double width = (3.0/4)*length;
+				   
+				   System.out.println("Length of fence :"+length);
+				   System.out.println("Width of fence :"+width);
+				   
+				   
+				    
+					
+					}
+					}
+					
+					
+				
+				     
+					  
