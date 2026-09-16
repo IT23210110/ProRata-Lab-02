9ee60cc1dbbe7164dce2a23011dcfae6dad04ea9
IT23210110 <IT23210110@my.sliit.lk>
2026-09-16 21:38:46 +0530

    Add files via upload

---
+++ Q3/IT23210110Lab2Q3.java
@@ -0,0 +1,10 @@
+public class IT23210110Lab2Q3{
+      public static void main(String[] args){
+	    double sideA = 3;
+		double sideB = 4;
+		
+		double hypotenus = Math.sqrt((sideA*sideA)+(sideB*sideB));
+		
+		System.out.println("Length of Hypotenus :" +hypotenus);
+	  }
+	  } 
